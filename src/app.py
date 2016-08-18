087ed513be59f01e34d0df732a619b56475742fd
Majid Mohamadi Parvar <m.shutdown57@gmail.com>
2016-08-18 11:43:26 +0430

    heroku config

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+from src.users.views import bp_users
+
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+app.secret_key = app.config['SECRET_KEY']
+
+app.register_blueprint(bp_users, url_prefix='/users')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
---
+++ src/users/views.py
@@ -0,0 +1,4 @@
+from flask import Blueprint, render_template
+
+
+bp_users = Blueprint('users', __name__)
